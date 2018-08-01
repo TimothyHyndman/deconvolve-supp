6d0d20c59ed1a5af3b1c7ad69a6fccaca4aba247
Unknown <timothy.hyndman@gmail.com>
2018-08-01 11:20:40 +1000

    Add base MATLAB files

---
+++ deconvolve.m
@@ -0,0 +1,348 @@
+%% Deconvolution of data X
+
+function [Q,tt,tt1,tt2,normhatphiW] = deconvolve(W)
+%Basic parameters
+n = length(W);
+m = 10;
+
+%-------------------------------------------------------------
+%-------------------------------------------------------------
+% Precalculate phi_W
+%-------------------------------------------------------------
+%-------------------------------------------------------------
+
+length_tt=100;
+a=-8;
+b=8;
+tt=a:((b-a)/length_tt):b;
+[tt,tt1,tt2,hat_phi_W,sqrt_psi_hat_W,normhatphiW]=computephiW(tt,length_tt,W,n);
+
+%Choose kernel Weight
+weight_type = 'Epanechnikov';
+weight = KernelWeight(weight_type,tt);
+
+%-------------------------------------------------------------
+%-------------------------------------------------------------
+% Solve for xj,pj
+%-------------------------------------------------------------
+%-------------------------------------------------------------
+
+fmax = 10^6;   %Just a big number
+n_iterations = 5; %Setting this to 1 works perfectly almost all the time.
+n_var_iterations = 5;  %Usually gets there in first iteration but sometimes needs a few goes
+
+%Minimize difference in phase functions
+for i = 1:n_iterations
+    pjtest = unifrnd(0,1,[1,m]);
+    pjtest = pjtest/sum(pjtest);
+    xjtest=sort(unifrnd(min(W),max(W),[1,m]));
+    [pjnew,xjnew,fmaxnew,exitflag] = min_phase(m,W,pjtest,xjtest,tt,hat_phi_W,sqrt_psi_hat_W,weight);
+    %If our new answer is better than our old one then accept it
+    if fmaxnew < fmax && exitflag>0
+        fmax = fmaxnew;
+        pj = pjnew;
+        xj = xjnew;
+    end
+end
+
+%Calculate penalties once 
+dt = tt(2) - tt(1);
+integrand = insideintegral(tt,pj,xj,hat_phi_W,sqrt_psi_hat_W,weight);
+fmax0 = dt*sum(integrand);
+
+%Find initial value for varmin based on best solution for fmax
+varmin = var_objective([pj,xj]);
+
+for i = 1:n_var_iterations
+    pjtest = unifrnd(0,1,[1,m]);
+    pjtest = pjtest/sum(pjtest);
+    xjtest=sort(unifrnd(min(W),max(W),[1,m]));
+    [pjnew,xjnew,fval,exitflag] = min_var(m,W,pjtest,xjtest,fmax0,tt,hat_phi_W,sqrt_psi_hat_W,weight);
+    if fval < varmin && exitflag > 0
+        varmin = fval;
+        pj = pjnew;
+        xj = xjnew;
+    end   
+end
+
+fval2 = phase_objective([pj,xj],m,tt,hat_phi_W,sqrt_psi_hat_W,weight);
+if fval2/fmax0 < 0.999
+    display('T(Y) got smaller!')
+end
+
+[pj,xj] = simplify_masses(pj,xj);
+Q.Support = xj;
+Q.ProbWeights = pj;
+end
+
+%-------------------------------------------------------------
+%-------------------------------------------------------------
+% Local Functions
+%-------------------------------------------------------------
+%-------------------------------------------------------------
+
+function weight = KernelWeight(weight_type,x)
+    length_x = length(x);
+    switch weight_type
+        case 'Epanechnikov'
+            sig_x=-x(1)/2;
+            weight=0.75/(2*sig_x)*(1-(x/(2*sig_x)).^2);
+        case 'Uniform'
+            max_x=-x(1);
+            weight = zeros(1,length_x) + 1/(2*max_x);
+        case 'Triangular'
+            max_x = -x(1);
+            weight = -1/(max_x^2)*(abs(x)-max_x);
+        case 'Triweight'
+            max_x = -x(1);
+            weight = 35/(32*max_x)*(1 - (x/max_x).^2).^3;
+    end
+end
+
+function [tt,tt1,tt2,hat_phi_W,sqrt_psi_hat_W,normhatphiW]=computephiW(tt,length_tt,W,n)
+    OO=outerop(tt,W,'*');
+
+    %-----------------------------------------------
+    %Estimate empirical characersitic fucntion of W
+    %-----------------------------------------------
+
+    rehatphiW=sum(cos(OO),2)/n;
+    imhatphiW=sum(sin(OO),2)/n;
+    normhatphiW=sqrt(rehatphiW.^2+imhatphiW.^2);
+
+    %------------------------------------------------------------
+    %t^*: Keep only t-values such that |\hat_phi_W|< n^{-0.25}
+    %------------------------------------------------------------
+
+    tmp=tt(normhatphiW<n^(-0.25));
+    %Refine the interval of t-values accordingly
+    tt1=max(tmp(tmp<0));
+    tt2=min(tmp(tmp>0));
+    if isempty(tmp(tmp<0))
+        tt1=min(tt);
+    end
+
+    if isempty(tmp(tmp>0))
+        tt2=max(tt);
+    end
+    
+    tt=tt1:(tt2-tt1)/length_tt:tt2; %Refined interval of t-values 
+    hat_phi_W = 0;
+    for i=1:n
+        hat_phi_W = hat_phi_W + exp(1i*tt*W(i));
+    end
+    hat_phi_W = (1/n)*hat_phi_W;
+    sqrt_psi_hat_W = abs(hat_phi_W);
+end
+
+function y=outerop(a,b,operator)
+    if nargin<3
+        operator='+';                       % for only two arguments assume outerproduct 
+    end  
+
+    if isequal(operator,'*')                % common operator 
+        y=a(:)*b(:)';
+    else    
+      outera=a(:)*ones(1,length(b));        % these are the matrices that 
+      outerb=ones(length(a),1)*b(:).';      % meshgrid(A,B) would generate 
+      functionHandle=str2func(operator);  % new R14 functionality
+      y=functionHandle(outera,outerb);    % allows faster/neater method
+    end
+end
+
+%------------------------%
+
+function [pj,xj,fval,exitflag] = min_var(m,W,pj,xj,fmax0,t,hat_phi_W,sqrt_psi_hat_W,weight)
+    x = [pj,xj];
+    options = optimoptions('fmincon','Display','off','Algorithm','active-set','TolFun',1e-6); 
+    %options = optimoptions('fmincon','Display','off','Algorithm','interior-point','TolFun',1e-6,'TolCon',1e-5);
+    %options = optimoptions('fmincon','Display','off','Algorithm','sqp','TolFun',1e-6);
+
+    %Setup constraints in form A*x<b. We need that entries of pj are
+    %non-negative
+    A = zeros(2*m);
+    A(1:m,1:m) = -eye(m);
+    b = zeros(2*m,1);
+
+    %Optional: The xj are increasing
+    for i = 1:m-1
+        A(m+i,m+i) = 1;
+    end
+    for i = 1:m-1
+        A(m+i,m+i+1) = -1;
+    end
+    b(2*m) = 1;
+
+    %Setup constraints in the form Aeq*x = beq. We need that the pj add up to one.
+    Aeq = zeros(2*m);
+    beq = zeros(2*m,1);
+    Aeq(2*m,1:m) = ones(1,m);
+    beq(2*m) = 1;
+
+    %Setup bounds lb<x<ub
+    %Masses
+    lb = zeros(2*m,1);
+    ub = ones(2*m,1);
+    %Roots
+    lb(m+1:2*m) = min(W);
+    ub(m+1:2*m) = max(W);
+    func = @(x) var_objective(x);
+    nonlcon = @(x)phaseconstraint(x,m,fmax0,t,hat_phi_W,sqrt_psi_hat_W,weight);
+    [x,fval,exitflag] = fmincon(func,x,A,b,Aeq,beq,lb,ub,nonlcon,options);
+
+    pj = x(1:m);
+    xj = x(m+1:2*m);
+end
+
+function fval = var_objective(x)
+    m = length(x)/2;
+    pj = x(1:m);
+    xj = x(m+1:2*m);
+    fval = sum(pj.*(xj.^2)) - (sum(pj.*xj))^2;
+end
+
+function [c,ceq]=phaseconstraint(x,m,fmax0,t,hat_phi_W,sqrt_psi_hat_W,weight)
+    pj = x(1:m);
+    xj = x(m+1:2*m);
+
+    %My own integral thingo
+    dt = t(2) - t(1);
+    integrand = insideintegral(t,pj,xj,hat_phi_W,sqrt_psi_hat_W,weight);
+    Tp = dt*sum(integrand);
+
+    %Finish off
+    tol = 0;
+    %c=[Tp - fmax0 - tol; penalty1-fmax1 - tol ; penalty2 - fmax2-tol];
+    c = Tp - fmax0 - tol;
+    ceq=[];
+end
+
+%------------------------%
+
+function [pj,xj,fval,exitflag] = min_phase(m,W,pj,xj,t,hat_phi_W,sqrt_psi_hat_W,weight)
+    x = [pj,xj];
+    options = optimoptions('fmincon','Display','off','Algorithm','active-set','TolFun',1e-6); 
+    %options = optimoptions('fmincon','Display','off','Algorithm','interior-point','TolFun',1e-6);
+    %options = optimoptions('fmincon','Display','off','Algorithm','sqp','TolFun',1e-6);
+
+    %Setup constraints in form A*x<b. We need that entries of pj are
+    %non-negative
+    A = zeros(2*m);
+    A(1:m,1:m) = -eye(m);
+    b = zeros(2*m,1);
+
+    %Optional: The xj are increasing
+    for i = 1:m-1
+        A(m+i,m+i) = 1;
+    end
+    for i = 1:m-1
+        A(m+i,m+i+1) = -1;
+    end
+    b(2*m) = 1;
+
+    %Setup constraints in the form Aeq*x = beq. We need that the pj add up to one.
+    Aeq = zeros(2*m);
+    beq = zeros(2*m,1);
+    Aeq(2*m,1:m) = ones(1,m);
+    beq(2*m) = 1;
+
+    %Setup bounds lb<x<ub
+    lb = zeros(2*m,1);
+    lb(m+1:2*m) = min(W);
+    ub = ones(2*m,1);
+    ub(m+1:2*m) = max(W);
+
+    func = @(x)phase_objective(x,m,t,hat_phi_W,sqrt_psi_hat_W,weight);
+    [x,fval,exitflag] = fmincon(func,x,A,b,Aeq,beq,lb,ub,[],options);
+
+    pj = x(1:m);
+    xj = x(m+1:2*m);
+end
+
+function [fval,penalty1,penalty2,Tp] = phase_objective(x,m,tt,hat_phi_W,sqrt_psi_hat_W,weight)
+    %Extract weights and roots
+    pj = x(1:m);
+    xj = x(m+1:2*m);
+
+    %Integrate
+    dt = tt(2) - tt(1);
+    integrand = insideintegral(tt,pj,xj,hat_phi_W,sqrt_psi_hat_W,weight);
+    Tp = dt*sum(integrand);
+
+    %Add penalty terms
+    % [penalty1,penalty2] = penalties(pj,xj,tt,hat_phi_W);  %My code is working
+    % without these!! :)
+    penalty1 = 0;
+    penalty2 = 0;
+    fval = Tp+penalty1+penalty2;
+end
+
+function integrand = insideintegral(t,pj,xj,hat_phi_W,sqrt_psi_hat_W,weight)
+
+    %Calculate characteristic function of our discrete distribution
+    m = length(pj);
+    phi_p = 0;
+    for i=1:m
+        phi_p = phi_p + (pj(i)*exp(1i*t*xj(i)));
+    end
+    %phi_p = conj(pj*(exp(1i*t'*xj)')); %Slightly Slower
+
+    %Calculate integrand
+    %fred = hat_phi_W - sqrt_psi_hat_W.*phi_p./abs(phi_p);  %Slightly slower
+    fred = hat_phi_W.*conj(phi_p) - abs(phi_p).*sqrt_psi_hat_W;
+
+    integrand = abs(fred).^2.*weight;
+end
+
+%------------------------%
+
+function [pj,xj] = simplify_masses(pj,xj)
+    zero_tolerance = 0.0001;
+    search_size = 0.01;
+
+    %Get rid of zeros
+    index = 1;
+    looping = true;
+    while looping
+        if pj(index) < zero_tolerance
+            xj(index:end-1) = xj(index+1:end);
+            xj(end) = [];
+            pj(index:end-1) = pj(index+1:end);
+            pj(end) = [];
+        else
+            index = index+1;
+        end
+        if index >= length(xj)
+            looping = false;
+        end
+    end
+
+    if pj(end) < zero_tolerance
+        pj(end) = [];
+        xj(end) = [];
+    end
+
+    %Get rid of duplicates
+    index = 1;
+    looping = true;
+    if length(xj) < 2
+        looping = false;
+    end
+    while looping
+        if abs(xj(index) - xj(index+1)) < search_size
+            xj(index) = (xj(index)*pj(index)+xj(index+1)*pj(index+1))/(pj(index)+pj(index+1));  %Weighted average location
+            xj(index+1:end-1) = xj(index+2:end);
+            pj(index) = pj(index)+pj(index+1);
+            pj(index+1:end-1) = pj(index+2:end);
+            pj(end) = [];
+            xj(end) = [];
+        else
+            index = index+1;
+        end
+        if index == length(xj)
+            looping = false;
+        end
+    end
+
+    pj = pj/sum(pj);    %Normalise
+end
---
+++ plotdeconvolution.m
@@ -0,0 +1,196 @@
+%% Plot deconvolution stuff
+
+function plotdeconvolution(W,Q,tt,tt1,tt2,normhatphiW,truedens)
+pj = Q.ProbWeights;
+xj = Q.Support;
+n = length(W);
+
+%Find density from discrete distribution
+xx = linspace(min(W),max(W),1000);
+[fW,xW]=ksdensity(W,xx);
+fXdeconvoluted = makesmooth(xW,tt,tt1,tt2,xj,pj,n,W,normhatphiW);
+
+%Plot stuff
+hX = plot(xW,truedens(xW),'g','LineWidth',2);
+hold on
+hW = plot(xW,fW,'b','LineWidth',2);
+hj = scatter(xj,pj,'m','filled');
+plot(xW,fXdeconvoluted,'m','LineWidth',2)
+hold off
+legend([hX hW hj],{'f_X','f_W','Our estimate for f_X'},'FontSize',20,'FontWeight','bold')
+axis([min(xW) max(xW) 0 1])
+end
+
+function fXdecc = makesmooth(xx,tt,tt1,tt2,xgrid,psol,n,W,normhatphiW)
+    dx=xx(2)-xx(1);
+
+    %----------------------------
+    % Estimate phi_X and phi_U
+    %----------------------------
+
+    [rephip,imphip,normphip]=computephiX(tt,xgrid,psol);
+    hatphiU=normhatphiW./normphip;
+
+    %----------------------------------------------------------------------------------------
+    %Estimate var(U): approximate phi_U by poly of degree 2, and estimate varU by -2 phi_U''
+    %----------------------------------------------------------------------------------------
+
+    %For this we use a finer grid than the grid tt
+    ttBB=tt1:(tt2-tt1)/200:tt2;
+    OO=outerop(ttBB,W,'*');
+
+    %Estimate empirical characersitic function of W
+    rehatphiWBB=sum(cos(OO),2)/n;
+    imhatphiWBB=sum(sin(OO),2)/n;
+
+    clear OO;
+    normhatphiWBB=sqrt(rehatphiWBB.^2+imhatphiWBB.^2);
+    clear rehatphiWBB imhatphiWBB;
+
+    [rephipBB,imphipBB,normphipBB]=computephiX(ttBB,xgrid,psol);
+    hatphiUBB=normhatphiWBB./normphipBB;
+
+    tvec=ttBB(hatphiUBB'>=1-0.05);
+    phiUtvec=hatphiUBB(hatphiUBB'>=1-0.05);
+    pp=polyfit(tvec,phiUtvec',2);
+    hatvarU=-2*pp(1);
+
+    %Then compute density estimator as indicated in the paper
+
+    tlim=[min(tt),max(tt)];
+
+    %Adjust estimator of phi_U as recommended in the paper
+    ppphiU=spline(tt,hatphiU);
+
+    %Compute bandwidth as recommended in the paper
+
+    hPIc=PI_deconvUestth4(W,tlim,ppphiU,hatvarU);
+
+    %Compute density estmator
+    fXdecc=fXKernDec2(xx,hPIc,W,tlim,ppphiU,hatvarU);
+
+    %Remove negative parts and rescale to integrate to 1
+    fXdecc(fXdecc<0)=0*fXdecc(fXdecc<0);
+    fXdecc=fXdecc/sum(fXdecc)/dx;
+end
+
+function [rephip,imphip,normphip]=computephiX(tt,xgrid,psol)
+    OO=outerop(tt,xgrid,'*');
+    pmat=repmat(psol,length(tt),1);
+    cosO=cos(OO).*pmat;
+    sinO=sin(OO).*pmat;
+    clear OO;
+
+    rephip=sum(cosO,2);
+    imphip=sum(sinO,2);
+    normphip=sqrt(rephip.^2+imphip.^2);
+end
+
+function y=outerop(a,b,operator)
+    if nargin<3
+        operator='+';                       % for only two arguments assume outerproduct 
+    end  
+
+    if isequal(operator,'*')                % common operator 
+        y=a(:)*b(:)';
+    else    
+      outera=a(:)*ones(1,length(b));        % these are the matrices that 
+      outerb=ones(length(a),1)*b(:).';      % meshgrid(A,B) would generate 
+      functionHandle=str2func(operator);  % new R14 functionality
+      y=functionHandle(outera,outerb);    % allows faster/neater method
+    end
+end
+
+function fXdec=fXKernDec2(xx,hPI,W,tlim,ppphiU,hatvarU)
+	phiK = @(t) (1-t.^2).^3;
+	muK2 = 6;
+
+	dt = .0002;
+	t = (-1:dt:1)';
+	t=reshape(t,length(t),1);
+	
+	phiU=phiUspline(t/hPI,hatvarU,tlim,ppphiU);
+	OO=outerop(t/hPI,W,'*');
+	%Estimate empirical characersitic fucntion of W
+	
+	n=length(W);
+	rehatphiW=sum(cos(OO),2)/n;
+	imhatphiW=sum(sin(OO),2)/n;
+
+	rephip=rehatphiW./phiU;
+	imphip=imhatphiW./phiU;
+
+	xt=outerop(t/hPI,xx,'*');
+	fXdec=cos(xt).*repmat(rephip,1,length(xx))+sin(xt).*repmat(imphip,1,length(xx));
+	fXdec=sum(fXdec.*repmat(phiK(t),1,length(xx)),1)/(2*pi)*dt/hPI;
+end
+
+function hPI = PI_deconvUestth4(W,tlim,ppphiU,hatvarU)
+    phiK = @(t) (1-t.^2).^3;
+    muK2 = 6;
+    n = length(W);
+
+    %grid of h values where to search for a solution
+    maxh=(max(W)-min(W))/10;
+    hnaive=1.06*sqrt(var(W))*n^(-1/5);
+    hgrid=hnaive/3:(maxh-hnaive/3)/100:maxh;
+
+    lh = length(hgrid);
+
+    stdevx = max(sqrt(var(W) - hatvarU),1/n); % std(X)
+    th4 = stdevx^(-9)*105/(32*sqrt(pi)); % Estimate theta4 by NR     
+
+    dt = .0002;
+    t = (-1:dt:1)';
+    t=reshape(t,length(t),1);
+    hgrid=reshape(hgrid,1,lh);
+
+    toverh=t*(1./hgrid);
+
+    phiK2=(phiK(t)).^2;
+    phiU2=phiUspline(toverh,hatvarU,tlim,ppphiU).^2;
+
+    rr=3;
+    % Find h3 for th3
+    term1= -hgrid.^2*muK2*th4;
+    term2=repmat(t.^(2*rr).*phiK2,1,lh)./phiU2;
+    term2=sum(term2,1)*dt./(2*pi*n*hgrid.^(2*rr+1));
+
+    ABias2 = (term1 + term2).^2;
+    indh3=find(ABias2==min(ABias2),1,'first');
+    h3 = hgrid(indh3);
+
+    OO=outerop(t/h3,W,'*');
+    %Estimate empirical characersitic fucntion of W
+    rehatphiW=sum(cos(OO),2)/n;
+    imhatphiW=sum(sin(OO),2)/n;
+    clear OO;
+    normhatphiW2=rehatphiW.^2+imhatphiW.^2;
+    th3 = sum(t.^(2*rr) .* normhatphiW2 .* phiK2 ./ phiU2(:,indh3))*dt/(2*pi*h3^(2*rr+1));
+
+    rr=2;
+    % Find h2 for th2
+    term1= -hgrid.^2*muK2*th3;
+    term2=repmat(t.^(2*rr).*phiK2,1,lh)./phiU2;
+    term2=sum(term2,1)*dt./(2*pi*n*hgrid.^(2*rr+1));
+
+    ABias2 = (term1 + term2).^2;
+    indh2=find(ABias2==min(ABias2),1,'first');
+    h2 = hgrid(indh2);
+
+    OO=outerop(t/h2,W,'*');
+    %Estimate empirical characersitic fucntion of W
+    rehatphiW=sum(cos(OO),2)/n;
+    imhatphiW=sum(sin(OO),2)/n;
+    clear OO;
+    normhatphiW2=rehatphiW.^2+imhatphiW.^2;
+    th2 = sum(t.^(2*rr) .* normhatphiW2 .* phiK2 ./ phiU2(:,indh2))*dt/(2*pi*h2^(2*rr+1));
+
+    term1=hgrid.^4*muK2^2*th2/4;
+    term2=repmat(phiK2,1,lh)./phiU2;
+    term2=sum(term2,1)*dt./(2*pi*n*hgrid);
+    AMISE=term1+term2;
+
+    indh=find(AMISE==min(AMISE),1,'first');
+    hPI = hgrid(indh);
+end
